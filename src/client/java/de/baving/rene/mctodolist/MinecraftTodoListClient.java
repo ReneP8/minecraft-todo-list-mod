19323ffd53c0b4f355058101984b98f42bafcfb8
ReneP8 <rene-baving@web.de>
2024-12-04 21:35:56 +0100

    fix: displaying only active tasks on the hud

--- src/client/java/de/baving/rene/mctodolist/MinecraftTodoListClient.java
+++ src/client/java/de/baving/rene/mctodolist/MinecraftTodoListClient.java
@@ -1,5 +1,6 @@
 package de.baving.rene.mctodolist;
 
+import de.baving.rene.mctodolist.data.Task;
 import de.baving.rene.mctodolist.screen.TodoScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -46,26 +47,29 @@ public class MinecraftTodoListClient implements ClientModInitializer {
         int xPosition = 10; // Fixed x position on the left side
         int yPosition = screenHeight / 2 - 50; // Centered vertically
 
-        // Render the "Tasks:" label only if there are tasks
-        if (!taskManager.getTasks().isEmpty()) {
+        // Render the "Tasks:" label only if there are undone tasks
+        if (taskManager.getTasks().stream().anyMatch(task -> !task.isDone())) {
             drawContext.drawTextWithShadow(client.textRenderer, "Tasks:", xPosition, yPosition, 0xFFFFFF);
 
-            // Render each task dynamically, but limit to a maximum of 5 tasks
+            // Render each undone task dynamically, but limit to a maximum of 5 tasks
             int yOffset = yPosition + 15; // Starting y position for tasks
             int maxTasksToShow = 5; // Maximum number of tasks to show
             int maxTextLength = 30; // Maximum length for task text
 
-            for (int i = 0; i < Math.min(taskManager.getTasks().size(), maxTasksToShow); i++) {
-                String task = taskManager.getTasks().get(i).getTitle();
-                String displayText = task;
+            int displayedTasks = 0;
+            for (Task task : taskManager.getTasks()) {
+                if (!task.isDone() && displayedTasks < maxTasksToShow) {
+                    String displayText = task.getTitle();
 
-                // Truncate the text if it exceeds the maximum length
-                if (task.length() > maxTextLength) {
-                    displayText = task.substring(0, maxTextLength - 3) + "...";
+                    // Truncate the text if it exceeds the maximum length
+                    if (displayText.length() > maxTextLength) {
+                        displayText = displayText.substring(0, maxTextLength - 3) + "...";
+                    }
+
+                    drawContext.drawTextWithShadow(client.textRenderer, "- " + displayText, xPosition, yOffset, 0xFFFFFF);
+                    yOffset += 10; // Move to the next line
+                    displayedTasks++;
                 }
-
-                drawContext.drawTextWithShadow(client.textRenderer, "- " + displayText, xPosition, yOffset, 0xFFFFFF);
-                yOffset += 10; // Move to the next line
             }
         }
     }
